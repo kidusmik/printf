4f46dca01fc183dd5a58dce6d9710284204158be
Samie <samrasolomon40@gmail.com>
2021-10-05 20:33:04 +0000

    Modified a content of main.h

--- main.h
+++ main.h
@@ -1,7 +1,35 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-int _printf(const char *format, ...);
+#include <unistd.h>
+#include <stdarg.h>
+/**
+* struct steps- structure created
+*
+* @spec: chooses a specifier from array of structure
+*
+* @f: function pointer
+*/
+typedef struct steps
+{
+char *spec;
+int (*f)(va_list arg);
+} specifier;
 
-#endif /* MAIN_H */
+
+
+int _printf(const char *format, ...);
+int print_char(char c);
+int converter(specifier sp[], const char *format, va_list arg);
+int print_c(va_list arg);
+int print_string(va_list arg);
+int print_percent(va_list arg);
+int print_unsigned(va_list arg);
+int print_integer(va_list arg);
+int print_HEX(va_list arg);
+int print_hex(va_list arg);
+int print_octal(va_list arg);
+int print_binary(va_list arg);
+int len_buffer(int n, int base);
+
+#endif
