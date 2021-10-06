6d256b3ebe5488d6c74c366b8cc1c399d06ac6ef
Samie <samrasolomon40@gmail.com>
2021-10-06 14:17:17 +0000

    Modified converter.c

--- converter.c
+++ converter.c
@@ -19,36 +19,31 @@ int converter(specifier sp[], const char *format, va_list arg)
 	{
 		if (format[i] == '%')
 		{
-			i++;
+		i++;
 			for (j = 0; sp[j].spec != NULL; j++)
 			{
 				if (format[i] == *sp[j].spec)
-				{
 					k = sp[j].f(arg);
 					if (k == -1)
-					{
 						return (-1);
-					}
 					len += k;
 					break;
-				}
-				if (sp[j].spec == NULL && format[i] != ' ')
+			}
+			if (sp[j].spec == NULL)
+			{
+				if (format[i] != '\0' && format[i] != ' ')
 				{
-					if (format[i] != '\0')
-					{
-						print_char('%');
-						print_char(format[i]);
-						len += 2;
-					}
-					return (-1);
+					print_char('%');
+					print_char(format[i]);
+					len += 2;
 				}
+				else
+					return (-1);
 			}
 		}
 		else
-		{
 			print_char(format[i]);
 			len++;
-		}
 	}
 	return (len);
 }
